9ca1d7123266cb9048caeedfb9ca7077d179dadd
Marc Hidalgo <marc@theleftbit.com>
2023-06-01 18:06:18 +0200

    Clean up

--- WembleyFilms/ViewController.swift
+++ WembleyFilms/ViewController.swift
@@ -1,7 +1,4 @@
 //
-//  ViewController.swift
-//  WembleyFilms
-//
 //  Created by Marc Hidalgo on 1/6/23.
 //
 
@@ -9,15 +6,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func loadView() {
         super.loadView()
         view = UIView()
         view.backgroundColor = .red
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
 
