fba663f874339d6d6c283998b5f38f89bafbb379
Marc Hidalgo <marc@theleftbit.com>
2023-06-01 17:54:31 +0200

    Remove storyboard

--- WembleyFilms/ViewController.swift
+++ WembleyFilms/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func loadView() {
+        super.loadView()
+        view = UIView()
+        view.backgroundColor = .red
+    }
 }
 
